4b970941149e6c6dcc115b916919279f698302c2
이동준 <leedongjun27@gmail.com>
2022-01-07 07:54:17 +0900

    allowed update host

--- webproj/settings.py
+++ webproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g79ta69t70fvrd#tyhr%yp2f8)tiqqz_e-o_584@qu(e-t!nvl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
