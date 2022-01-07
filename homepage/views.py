71d932dd2cb421c978960b777ff2fe07f00d95c7
이동준 <leedongjun27@gmail.com>
2022-01-07 21:56:18 +0900

    change index

--- homepage/views.py
+++ homepage/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello World")
-    return render(request, 'index.html', {})
+    return render(request, 'projeda.html', {})
 
 def result(request):
     return render(request, 'jupyter1.html',{})
