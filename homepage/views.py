c43265a81b0645bf5c59ab9f25d8698cb338bfc1
이동준 <leedongjun27@gmail.com>
2022-01-07 16:47:07 +0900

    jupyter.html

--- homepage/views.py
+++ homepage/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, HttpResponse
 def index(request):
     # return HttpResponse("Hello World")
     return render(request, 'index.html', {})
+
+def abc(request):
+    return render(request, 'jupyter.html')
--- webproj/urls.py
+++ webproj/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from homepage.views import index
+from homepage.views import index,abc
 
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
+    path('abc', abc)
 ]
